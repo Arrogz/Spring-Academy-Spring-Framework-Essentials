83ae762de57c14d1926c50e84044e99e065d3705
Arrogz <108223317+Arrogz@users.noreply.github.com>
2026-09-21 14:07:01 +1000

    Todo11 Done

--- lab/12-javaconfig-dependency-injection/src/test/java/rewards/RewardNetworkTests.java
+++ lab/12-javaconfig-dependency-injection/src/test/java/rewards/RewardNetworkTests.java
@@ -4,6 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import rewards.AccountContribution;
+import rewards.Dining;
+import rewards.RewardConfirmation;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.RewardRepository;
+
+import common.money.MonetaryAmount;
+
 import rewards.TestInfrastructureConfig;
 
 public class RewardNetworkTests {
@@ -22,6 +35,31 @@ public class RewardNetworkTests {
     @Test
     void testRewardForDining() {
         
+		Dining dining = Dining.createDining("100.00", "1234123412341234", "1234567890");
+
+		// call the 'rewardNetwork' to test its rewardAccountFor(Dining) method
+		RewardConfirmation confirmation = rewardNetwork.rewardAccountFor(dining);
+
+		// assert the expected reward confirmation results
+		assertNotNull(confirmation);
+		assertNotNull(confirmation.getConfirmationNumber());
+
+		// assert an account contribution was made
+		AccountContribution contribution = confirmation.getAccountContribution();
+		assertNotNull(contribution);
+
+		// the account number should be '123456789'
+		assertEquals("123456789", contribution.getAccountNumber());
+
+		// the total contribution amount should be 8.00 (8% of 100.00)
+		assertEquals(MonetaryAmount.valueOf("8.00"), contribution.getAmount());
+
+		// the total contribution amount should have been split into 2 distributions
+		assertEquals(2, contribution.getDistributions().size());
+
+		// each distribution should be 4.00 (as both have a 50% allocation)
+		assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Annabelle").getAmount());
+		assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Corgan").getAmount());
     }
     
 }
