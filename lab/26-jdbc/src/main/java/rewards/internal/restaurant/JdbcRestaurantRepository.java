75e585981fd23f463f7a866cd5fa6efcd8f40a11
Arrogz <108223317+Arrogz@users.noreply.github.com>
2026-09-23 17:50:11 +1000

    Implementing JdbcTemplate

--- lab/26-jdbc/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
+++ lab/26-jdbc/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
@@ -2,6 +2,7 @@ package rewards.internal.restaurant;
 
 import common.money.Percentage;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.Dining;
 import rewards.internal.account.Account;
 
@@ -40,27 +41,40 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 
 	private DataSource dataSource;
 
+	private JdbcTemplate jdbcTemplate;
+
 	public JdbcRestaurantRepository(DataSource dataSource) {
 		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public Restaurant findByMerchantNumber(String merchantNumber) {
 		String sql = "select MERCHANT_NUMBER, NAME, BENEFIT_PERCENTAGE, BENEFIT_AVAILABILITY_POLICY"
 				+ " from T_RESTAURANT where MERCHANT_NUMBER = ?";
-		Restaurant restaurant = null;
+		//Restaurant restaurant = null;
 
-		try (Connection conn = dataSource.getConnection();
-			 PreparedStatement ps = conn.prepareStatement(sql) ){
-			ps.setString(1, merchantNumber);
-			ResultSet rs = ps.executeQuery();
-			advanceToNextRow(rs);
-			restaurant = mapRestaurant(rs);
-		} catch (SQLException e) {
-			throw new RuntimeException("SQL exception occurred finding by merchant number", e);
-		}
+		// try (Connection conn = dataSource.getConnection();
+		// 	 PreparedStatement ps = conn.prepareStatement(sql) ){
+		// 	ps.setString(1, merchantNumber);
+		// 	ResultSet rs = ps.executeQuery();
+		// 	advanceToNextRow(rs);
+		// 	restaurant = mapRestaurant(rs);
+		// } catch (SQLException e) {
+		// 	throw new RuntimeException("SQL exception occurred finding by merchant number", e);
+		// }
 
 		return restaurant;
 	}
+
+		
+	// - Refactor findByMerchantNumber(..) to use the JdbcTemplate and a RowMapper
+	//
+	//   #1: Create a RowMapper object and pass it to the
+	//       jdbcTemplate.queryForObject(..) method as an argument
+	//	 #2: The mapRestaurant(..) method provided in this class contains
+	//	     logic, which the RowMapper may wish to use
+	//
+	// - Run JdbcRestaurantRepositoryTests again and verity it passes
 
 	/**
 	 * Maps a row returned from a query of T_RESTAURANT to a Restaurant object.
